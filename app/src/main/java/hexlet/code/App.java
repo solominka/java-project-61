6df313cd44d9eeb1c722dddd392deed9259fe43f
introvertess <solominka21veka@gmail.com>
2024-09-24 17:21:27 +0300

    IT10: add welcome phrase

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,6 +12,7 @@ public class App {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - " + CheckEvenGame.GAME_NAME);
